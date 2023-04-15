1a2b2c89221866e50166c8ccd2314554f7884ee0
Manie Kansal <124429456+ManieKGupta@users.noreply.github.com>
2023-04-15 16:29:28 +0530

    Add files via upload

---
+++ Basic projects/Blynk_termpcontrol.ino
@@ -0,0 +1,62 @@
+#define BLYNK_PRINT Serial
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#include <SimpleTimer.h>
+#include <dht.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "k3ru6AD4whWrIuQb-_1a2ydAHxFp-eqd";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "jeraths";
+char pass[] = "aditya2k";
+
+#define DHTPIN D2          // What digital pin we're connected to
+dht DHT;
+// Uncomment whatever type you're using!
+//#define DHTTYPE DHT11     // DHT 11
+//#define DHTTYPE DHT22   // DHT 22, AM2302, AM2321
+//#define DHTTYPE DHT21   // DHT 21, AM2301
+int alarmPin = 4;
+int led1 = 5;
+int led2 = 13;
+
+SimpleTimer timer;
+
+void sendSensor(){
+  int chk= DHT.read11(D2);
+  float h = DHT.humidity;
+  float t = DHT.temperature; // or dht.readTemperature(true) for Fahrenheit
+
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  Serial.println(t);
+  Blynk.virtualWrite(V5, h);
+  Blynk.virtualWrite(V6, t);
+
+  // SETUP the ALARM Trigger and Send EMAIL 
+  // and PUSH Notification
+
+  if(t > 25){
+    Blynk.email("jerathhimani29@gmail.com", "ESP8266 Alert", "Temperature over 28C!");
+    //Blynk.notify("ESP8266 Alert - Temperature over 28C!");
+  }
+}
+
+void setup(){
+  Serial.begin(115200);
+  Blynk.begin(auth, ssid, pass);
+//  DHT.begin();
+  timer.setInterval(2500L, sendSensor);
+}
+
+void loop(){
+  Blynk.run();
+timer.run();
+}
+
---
+++ Basic projects/joystick.ino
@@ -0,0 +1,39 @@
+#include <Servo.h> // include servo library
+
+Servo servo1; // define servos
+//Servo servo2;
+
+int joyX = 0; // give variable to joystick readings
+int joyY = 1;
+
+int joyVal;
+int joyVal1;
+// create variable for joystick value
+
+void setup()
+{
+servo1.attach(3); // start servos
+//servo2.attach(4);
+
+
+
+}
+
+
+void loop()
+{
+  
+joyVal = analogRead(joyX); // read value from joystick
+joyVal = map(joyVal, 0, 1023, 0, 180); // change value range to 0-180
+servo1.write(joyVal); // write value to servo
+
+joyVal1 = analogRead(joyY); // repeat same for y axis
+joyVal1 = map(joyVal1, 0, 1023, 0, 180);
+servo1.write(joyVal1);
+delay(20); // add small delay to reduce noise
+
+
+
+  
+  
+}
